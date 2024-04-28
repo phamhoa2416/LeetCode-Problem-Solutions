627df129d47769691c915ae21f61fe0c0ed67458
phamhoa2416 <152608093+phamhoa2416@users.noreply.github.com>
2024-04-28 15:49:36 +0700

    Delete 0136-single-number/0136-single-number.java

--- 0136-single-number/0136-single-number.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int singleNumber(int[] nums) {
-        int result = 0;
-        for (int val : nums) {
-            result ^= val;
-        }  
-        return result;
-    }
-}
