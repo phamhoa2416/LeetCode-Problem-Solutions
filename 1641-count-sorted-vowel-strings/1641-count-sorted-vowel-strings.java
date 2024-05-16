4f3642598fc9d81b7da4f7f86b8cfad939cb975e
phamhoa2416 <152608093+phamhoa2416@users.noreply.github.com>
2024-05-16 09:15:52 +0700

    Time: 0 ms (100%), Space: 39.8 MB (97.91%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -1,12 +1,12 @@
 class Solution {
     public int countVowelStrings(int n) {
-        int[][] dp = new int[n][5];
+        int[][] dp = new int[n + 1][5];
 
         for (int j = 0; j < 5; ++j) {
-            dp[0][j] = j + 1;
+            dp[0][j] = 1;
         }
 
-        for (int i = 1; i < n; ++i) {
+        for (int i = 1; i < n + 1; ++i) {
             for (int j = 0; j < 5; ++j) {
                 if (j == 0) {
                     dp[i][j] = dp[i - 1][j];
@@ -16,6 +16,6 @@ class Solution {
             }
         }
 
-        return dp[n - 1][4];
+        return dp[n][4];
     }
 }
