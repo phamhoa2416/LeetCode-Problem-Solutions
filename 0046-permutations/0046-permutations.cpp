d897da23f3c32000f790364ff309c67f1e53272a
phamhoa2416 <152608093+phamhoa2416@users.noreply.github.com>
2024-10-02 19:50:05 +0700

    Time: 0 ms (100%), Space: 9 MB (87.03%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int length = nums.size();
+
+        int pivot = -1;
+        for (int i = length - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                pivot = i;
+                break;
+            }
+        }
+
+        if (pivot == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        for (int i = length - 1; i > pivot; i--) {
+            if (nums[i] > nums[pivot]) {
+                swap(nums[i], nums[pivot]);
+                break;
+            }
+        }
+
+        reverse(nums.begin() + pivot + 1, nums.end());
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> permutation(nums);
+        do {
+            nextPermutation(permutation);
+            res.push_back(permutation);
+        } while (permutation != nums);
+
+        return res;
+    }
+};
